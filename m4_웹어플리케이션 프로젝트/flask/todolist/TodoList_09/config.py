ab2c39872443ee619c3c9fbf30f0be9ee3d9fd2c
yk91412 <167948283+yk91412@users.noreply.github.com>
2024-07-18 15:09:52 +0900

    Add files via upload

---
+++ m4_웹어플리케이션 프로젝트/flask/todolist/TodoList_09/config.py
@@ -0,0 +1,4 @@
+SECRET_KEY = 'a4e404bb64b98ad5d42a72ae8c83ac3a10f2dce3270d10c0'
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///tasks.db'
+SQLALCHEMY_DATABASE_URI = 'mysql://user1:user1@localhost:3306/user2_db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
